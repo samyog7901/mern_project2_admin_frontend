45b0f7f8b35f93c2f0aa0487ed72333a10189f7e
samyog7901 <samyogkhadka247@gmail.com>
2025-12-07 20:50:51 +0545

    Added Bulk Product Upload feature

--- src/components/Tables/TableTwo.tsx
+++ src/components/Tables/TableTwo.tsx
@@ -51,7 +51,7 @@ const TableTwo = () => {
             </tr>
           </thead>
           <tbody>
-            {products.length> 0 && products.map((product, key) => (
+            {products.length > 0 && products.slice().sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()).slice(0, 10).map((product, key) => (
               <tr key={key}>
                 <td className="border-b border-[#eee] py-5 px-4 pl-9 dark:border-strokedark xl:pl-11 flex gap-4">
                 <div className="rounded-md">
@@ -159,63 +159,7 @@ const TableTwo = () => {
         </table>
       </div>
       
-{/* 
-      <div className="grid grid-cols-6 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5">
-        <div className="col-span-3 flex items-center">
-          <p className="font-medium">Product Name</p>
-        </div>
-        <div className="col-span-2 hidden items-center sm:flex">
-          <p className="font-medium">Category</p>
-        </div>
-        <div className="col-span-1 flex items-center">
-          <p className="font-medium">Price</p>
-        </div>
-        <div className="col-span-1 flex items-center">
-          <p className="font-medium">Stocks</p>
-        </div>
-        <div className="col-span-1 flex items-center">
-          <p className="font-medium">Created_at</p>
-        </div>
-        <div className="col-span-1 flex items-center">
-          <p className="font-medium">Actions</p>
-        </div>
-      </div> */}
 
-      {/* {products.length > 0 && products.map((product, key) => (
-        <div
-          className="grid grid-cols-6 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5"
-          key={key}
-        >
-          <div className="col-span-3 flex items-center">
-            <div className="flex flex-col gap-4 sm:flex-row sm:items-center">
-              <div className="h-12.5 w-15 rounded-md">
-                <img src={product.imageUrl} alt="Product" />
-              </div>
-              <p className="text-sm text-black dark:text-white">
-                {product.productName}
-              </p>
-            </div>
-          </div>
-          <div className="col-span-2 hidden items-center sm:flex">
-            <p className="text-sm text-black dark:text-white">
-              {product?.Category?.categoryName}
-            </p>
-          </div>
-          <div className="col-span-1 flex items-center">
-            <p className="text-sm text-black dark:text-white">
-              ${product.price}
-            </p>
-          </div>
-          <div className="col-span-1 flex items-center">
-            <p className="text-sm text-black dark:text-white">{product.stockQty}</p>
-          </div>
-          <div className="col-span-1 flex items-center">
-            <p className="text-sm text-meta-3">{product.createdAt
-    ? new Date(product.createdAt).toLocaleDateString()
-    : "N/A"}</p>
-          </div>
-        </div>
-      ))} */}
     </div>
   );
 };
